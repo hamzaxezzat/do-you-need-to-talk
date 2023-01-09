a64f2c7c6d7a8d31d8c12a8e0e68ff6305c886e2
hamza ezzat <hamza2007ezzat@gmail.com>
2023-01-09 19:24:05 +0100

    sign in/up form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,16 @@ import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
+import {ChannelListContainer,ChannelContainer, Auth} from "./components"
 import './App.css'
-
-
-import {ChannelListContainer,ChannelContainer} from "./components"
 
 
 const apiKey = 'eqf72rmabxcw';
 const client = StreamChat.getInstance(apiKey);
+const authToken = false;
 
 function App() {
+  if (!authToken) return <Auth />
   return (
    <div className="app__wrapper">
       <Chat client={client} theme="team light">
