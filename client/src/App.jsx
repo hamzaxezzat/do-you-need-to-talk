680a09709b4387cd77439ac47c0f2823cdd88b03
hamza ezzat <hamza2007ezzat@gmail.com>
2023-01-09 08:51:20 +0100

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = 'eqf72rmabxcw';
+
+function App() {
+  return (
+   <></>
+  );
+}
+
+export default App;
