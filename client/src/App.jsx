e9359929d1381a6b52976c15ad3dd340ee07d46a
hamza ezzat <hamza2007ezzat@gmail.com>
2023-01-09 10:13:07 +0100

    ✅ ChannelSeach Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,23 @@ import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
+import './App.css'
+
+
+import {ChannelListContainer,ChannelContainer} from "./components"
+
+
 const apiKey = 'eqf72rmabxcw';
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
   return (
-   <></>
+   <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+   </div>
   );
 }
 
