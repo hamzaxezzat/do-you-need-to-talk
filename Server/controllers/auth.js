57aaa197407b2a3b639eb836c46848db81544c37
hamza ezzat <hamza2007ezzat@gmail.com>
2023-01-30 09:30:42 +0100

    Start Server

---
+++ Server/controllers/auth.js
@@ -0,0 +1,4 @@
+const login = () => {};
+const signup = () => {};
+
+module.exports = {login,signup}
---
+++ Server/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const {signup, login} = require ('../controllers/auth')
+const router = express.Router();
+
+// Send Data from front-end to back-end
+router.post('/signup',signup);
+router.post('/login',login);
+
+
+module.exports = router
